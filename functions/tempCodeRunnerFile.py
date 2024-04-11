6983eccc2d4bc234643557fc2f5ca7b47002a16a
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-04-11 11:39:10 +0300

    feat: solve tasks to theme 2

--- functions/tempCodeRunnerFile.py
+++ functions/tempCodeRunnerFile.py
@@ -1,10 +1,13 @@
-
-def discount_price(price, discount):
-    def apply_discount():
-        nonlocal price
-        price = price*(1-discount)
-    apply_discount()
-    return price
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 
-print(discount_price(100, 0.2)) 
+def number_of_groups(n, k):
+    number_of_groups=factorial(n) / (factorial(n - k) * factorial(k))
+    print(int(number_of_groups))
+    return int(number_of_groups)
+
+number_of_groups(50, 7)
