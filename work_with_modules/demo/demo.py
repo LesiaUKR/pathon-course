5b4bca817a95665f2c43ee54625b20c314effbf6
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-04-28 23:32:48 +0300

    feat: work with pip

---
+++ work_with_modules/demo/demo.py
@@ -0,0 +1,13 @@
+# python -m venv <name of the virtual environment> - створення віртуального середовища
+# -m - запуск модуля як скрипта
+# venv, .env, env - назви для віртуальних середовищ
+
+# pip - Python package manager
+# pip3 list - список встановлених пакетів Python глобально
+
+# source .env/bin/activate - активація віртуального середовища
+# .\.env\Scripts\activate.ps1 - активація віртуального середовища в Windows
+# deactivate - вийти із віртуального середовища, опинимося в глобальному середовищі
+
+# pip3 show moskali - інформація про пакет
+# run_main - запустити головний файл
